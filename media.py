5560c6e84148b8ce062d66aa8ce4243f97a6f852
nerizinha <bneri932@gmail.com>
2026-08-26 12:43:18 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,10 @@
+nota1 = float(input("digite a nota 1:"))
+nota2 = float(input("digite a nota 2:"))
+nota3 = float(input("digite a nota 3:"))
+nota4 = float(input("digite a nota 4:"))
+
+media = (3 + 5 + 7 + 8) /4
+if media >=5:
+    print("aluno aprovado")
+else:
+    print("aluno reprovado")
