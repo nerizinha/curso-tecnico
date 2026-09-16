174ed9fab4e25d96c88bcd327318b5ecf49d6859
nerizinha <bneri932@gmail.com>
2026-09-16 12:52:24 -0300

    Add files via upload

---
+++ idoso.py
@@ -0,0 +1,9 @@
+idade = int(input("digite sua idade"))
+if idade >=60:
+    print("idoso")
+elif idade >=18:
+    print ("adulto")
+elif idade >=13:
+    print ("adolescente")
+else:
+    print ("criança")
--- media.py
+++ media.py
@@ -1,9 +1,13 @@
 nota1 = float(input("digite a nota 1:"))
 nota2 = float(input("digite a nota 2:"))
 nota3 = float(input("digite a nota 3:"))
+nota4 = float(input("digite a nota 4:"))
 
-media = (3 + 5 + 7 + 8) /4
-if media >=5:
-    print("aluno aprovado")
+media = (nota1 + nota2 + nota3 + nota4)/4
+print ("sua media é",media)
+if media <=3:
+    print("reprovado")
+elif media >=5:
+    print ("recuperação")
 else:
     print("aluno reprovado")
