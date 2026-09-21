9ec3a3192022b87be47df7cb7bd0770c6a832169
nerizinha <bneri932@gmail.com>
2026-09-21 13:04:37 -0300

    Add files via upload

---
+++ positivo.py
@@ -0,0 +1,7 @@
+numero = int(input("digite um numero:"))
+if numero >0:
+    print("numero positivo")
+elif numero ==0:
+    print("zero")
+else:
+    print("numero negativo")
---
+++ temperatura.py
@@ -0,0 +1,9 @@
+temperatura = int(input("digite um numero:"))
+if temperatura >=40:
+    print("muito quente")
+elif temperatura >=30:
+    print("quente")
+elif temperatura >=20:
+    print("fresco")
+else:
+    print("frio")
