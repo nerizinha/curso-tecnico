b6580a05550ac3985a421dc0ef254a20d722ed63
nerizinha <bneri932@gmail.com>
2026-09-23 12:59:48 -0300

    Add files via upload

--- numero.py
+++ numero.py
@@ -1,5 +1,13 @@
-numero = int(input("digite um numero:"))
-if numero >10:
-    print("o numero é maior que 10")
+opcao = int(input("escolha uma opção: "))
+if opcao ==1:
+    print("caixa um")
+elif opcao ==2:
+    print("caixa dois")
+elif opcao ==3:
+    print("caixa tres")
+elif opcao ==4:
+    print("caixa quatro")
+elif opcao ==5:
+    print("caixa cinco")
 else:
-    print("o numero é menor que 10")
+    print("opção invalida")
